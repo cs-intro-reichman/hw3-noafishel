44c70610cd4bed121bcc25b2df66dd1a3f12a6d3
Noa Fishel <noa.fishel@post.runi.ac.il>
2025-11-15 23:45:31 +0200

    Anagram completed

--- Anagram.java
+++ Anagram.java
@@ -28,22 +28,53 @@ public class Anagram {
 
 	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
-		// Replace the following statement with your code
-		return false;
+		str1 = preProcess(str1);
+		str2 = preProcess(str2);
+
+		if (str1.length() != str2.length()) return false;
+	
+		for (int i = 0; i < str1.length(); i++){
+			char c = str1.charAt(i);
+			int count1 = 0;
+			int count2 = 0;
+			for (int j = 0; j < str1.length(); j++){
+				if (c == str1.charAt(j)) {
+					count1++;
+				}
+				if (c == str2.charAt(j)){
+					count2++;
+				}
+			}
+			if (count1 != count2) return false;
+		}
+		return true;
 	}
 	   
 	// Returns a preprocessed version of the given string: all the letter characters are converted
 	// to lower-case, and all the other characters are deleted, except for spaces, which are left
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
 	public static String preProcess(String str) {
-		// Replace the following statement with your code
-		return "";
+		String newStr = "";
+		str = str.toLowerCase();
+		for (int i = 0; i < str.length(); i++){
+			char c = str.charAt(i);
+			if(Character.isLetter(c)){
+				newStr += c;
+			}
+		}
+		return newStr;
 	} 
 	   
 	// Returns a random anagram of the given string. The random anagram consists of the same
 	// characters as the given string, re-arranged in a random order. 
 	public static String randomAnagram(String str) {
-		// Replace the following statement with your code
-		return "";
+		StringBuilder strB = new StringBuilder(str);
+		String randomStr = "";
+		while (strB.length() > 0) {
+			int random = (int)(Math.random() * strB.length());
+			randomStr += strB.charAt(random);
+			strB.deleteCharAt(random);
+		}
+		return randomStr;
 	}
 }
